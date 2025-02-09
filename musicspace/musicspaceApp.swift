59c142f3709011f1b8e6479e60ab8dc2d4a2358a
bappelberg <benjamin.w.appelberg@gmail.com>
2025-02-09 14:47:05 +0100

    Initial Commit

---
+++ musicspace/musicspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicspaceApp.swift
+//  musicspace
+//
+//  Created by bappelberg on 2025-02-09.
+//
+
+import SwiftUI
+
+@main
+struct musicspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
